58f95889309a33c8fbdfde114cb19a608f7a5471
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-11-17 16:31:08 +0700

    Add files via upload

---
+++ Soal1.py
@@ -0,0 +1,5 @@
+phi = 3.14
+r= int(input("masukan panjang jari-jari lingkaran: "))
+luas = phi*r*r
+print ("Luas lingkaran adalah : "+ str(luas))
+
---
+++ Soal2.py
@@ -0,0 +1,5 @@
+apaan = int(input("Masukkan alas atap: "))
+apaantuh = int(input("Masukkan tinggi atap: "))
+hooh= int(input("Masukan tinggi tembok: "))
+persegi = apaan*apaantuh*hooh
+print ("Luas persegi adalah", persegi)
